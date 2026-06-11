24601c7ae5e69b2a1f7cff0c8cc9c07bae022c04
coredev-emersonbacomo <coredev.emersonbacomo@gmail.com>
2026-06-11 16:04:14 +0800

    feat: tailwindcss setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path";
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://server-monitoring-system.test',
-        changeOrigin: true,
-      },
+    plugins: [
+        react(),
+        tailwindcss(),
+        babel({ presets: [reactCompilerPreset()] }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://server-monitoring-system.test",
+                changeOrigin: true,
+            },
+        },
+    },
+});
