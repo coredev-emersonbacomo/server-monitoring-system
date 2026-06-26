7ed4c5bc13fbcfac45e5984aaa211f90dc977712
raysha23 <coredevraymundsismundo@gmail.com>
2026-06-26 16:31:11 +0800

    fix: frontend to backend, CRUD of the User Management

--- database/factories/ServerFactory.php
+++ database/factories/ServerFactory.php
@@ -15,7 +15,7 @@ class ServerFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => fn () => User::inRandomOrder()->first()?->id ?? User::factory(),
+            //'user_id' => fn () => User::inRandomOrder()->first()?->id ?? User::factory(),
             'client_id' => Client::inRandomOrder()->first()?->id ?? Client::factory(),
             'server_name' => fake()->domainWord() . '-prod',
             'device_name' => fake()->word() . '-blade-' . fake()->randomDigitNotNull(),
