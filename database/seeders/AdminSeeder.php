f730035450d42b6ee92e1ad1134cc93e80467f4d
raysha23 <coredevraymundsismundo@gmail.com>
2026-06-26 16:31:11 +0800

    fix: frontend to backend, CRUD of the User Management

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,7 +17,7 @@ class AdminSeeder extends Seeder
         DB::table("users")->updateOrInsert(
             ['username' => 'admin'],
             [
-                'uuid' => (string) Str::uuid7(),
+                
                 'first_name' => 'Admin',
                 'last_name'=> 'Surname',
                 'email' => 'admin@example.com',
