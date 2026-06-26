23ec742df00ca9fe313123831c13e818cdb85e27
coredev-Portrias <coredev.johannkarlportrias@example.com>
2026-06-26 14:48:11 +0800

    refactor: change column name of uuid's

--- database/migrations/2026_06_11_061126_create_clients_table.php
+++ database/migrations/2026_06_11_061126_create_clients_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->uuid('client_id')->unique();
+            $table->uuid('uuid')->unique();
             
             $table->string('name');
             $table->string('description')->nullable();
