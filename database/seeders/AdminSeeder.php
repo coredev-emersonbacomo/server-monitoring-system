b6358eae2feba5b9486a9d55377ab1ced9d8f23f
coredev-Portrias <coredev.johannkarlportrias@example.com>
2026-06-26 16:13:52 +0800

    refactor: changed contact_number to phone_number to User

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,15 +17,17 @@ class AdminSeeder extends Seeder
         DB::table("users")->updateOrInsert(
             ['username' => 'admin'],
             [
-                'uuid' => (string) Str::uuid7(),
+                
                 'first_name' => 'Admin',
                 'last_name'=> 'Surname',
                 'email' => 'admin@example.com',
-                'contact_number' => '1234567890',
+                'phone_number' => '1234567890',
                 'password' => bcrypt('admin123'),
                 'created_at' => now(),
                 'updated_at'=> now(),
                 'last_login' => now()
+                
+
             ]
         );
     }
