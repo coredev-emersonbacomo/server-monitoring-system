2df0bd7199645f6c706f84c2ff90422608b6cf34
coredev-Portrias <coredev.johannkarlportrias@example.com>
2026-06-29 12:41:21 +0800

    Merge conflicts resolved

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,7 +57,7 @@ class UserController extends Controller
         if (!($data->email instanceof \Spatie\LaravelData\Optional)) {
             $payload['email'] = $data->email;
         }
-        if (!($data->phone_number instanceof \Spatie\LaravelData\Optional)) {
+        if(!($data->phone_number instanceof \Spatie\LaravelData\Optional)) {
             $payload['phone_number'] = $data->phone_number;
         }
         if (!($data->username instanceof \Spatie\LaravelData\Optional)) {
