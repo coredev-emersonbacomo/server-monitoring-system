d46fad9ec6b9a3c47086e954b1c45cd04ce9bf3b
amonMID <anchorjave16@gmail.com>
2026-06-29 14:04:38 +0800

    fix: exception error Clientdetails.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -33,10 +33,10 @@ public function uniqueIds(): array
     }
     public function servers(): hasMany
     {
-        return $this->hasMany(Server::class, 'client_id');
+        return $this->hasMany(Server::class, 'client_id', 'uuid');
     }
     public function secopclients(): BelongsToMany
-    {
-        return $this->belongsToMany(User::class, 'sec_op_clients', 'client_id', 'user_id');
-    }
+{
+    return $this->belongsToMany(User::class, 'secop_client', 'client_id', 'user_id');
+}
 }
