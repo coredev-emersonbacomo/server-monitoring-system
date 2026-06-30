4b64199c9b0eb0a90e52b428918453d1ddf22366
coredevtim <coredev.timotheochepadilla@gmail.com>
2026-06-30 16:26:56 +0800

    chore: add server cpu_model and server dto

---
+++ app/Data/UpdateServerSpecsData.php
@@ -0,0 +1,18 @@
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+
+class UpdateServerSpecsData extends Data
+{
+    public function __construct(
+        public string $uuid,
+
+        public string $cpu_model,
+
+        public int $cpu_cores,
+
+        public int $ram,
+
+        public string $operating_system,
+    ) {}
+}
