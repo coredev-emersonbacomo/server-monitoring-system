e2a17925411fda10909abe8da61e5ee4503f7992
coredev-Portrias <coredev.johannkarlportrias@example.com>
2026-07-01 16:15:58 +0800

    refactor: changed client card description attributes

---
+++ app/Models/ClientAlert.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Client;
+
+class ClientAlert extends Model
+{
+   public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class, 'client_id');
+    }
+}
--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -11,12 +11,21 @@ class ClientFactory extends Factory
 
     public function definition(): array
     {
+        $bannerImages = [
+            'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://www.allplan.com/fileadmin/_processed_/4/7/csm_w57-image-by-nic-lehoux_original_a82f2b39f2.jpg',
+            'https://static.flickr.com/35/100571773_ec21b93bac_o.jpg',
+            'https://www.touropia.com/gfx/b/2009/09/burj_al_arab.jpg',
+            'https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Umesh%20Bhosale/Statheros/20-Stretheros-Umesh-Bhosale-Skyscraper.jpg'
+        ];
+
         return [
             'name' => fake()->company(),
             'description' => fake()->sentence(),
             'email' => fake()->unique()->companyEmail(),
             'contact_number' => fake()->numerify('09##########'),
             'location' => fake()->address(),
+            'banner_image_url' => fake()->randomElement($bannerImages),
         ];
     }
 }
